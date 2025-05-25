4b1889494d2dd26849ac949be0130e352a16a853
Olivier Sels <olivier.sels@gmail.com>
2025-05-25 21:32:16 +0200

    Add default dir reqrite to index.html

--- deployment/aws/lambda-edge/src/check-auth/index.ts
+++ deployment/aws/lambda-edge/src/check-auth/index.ts
@@ -24,6 +24,20 @@ export const handler: CloudFrontRequestHandler = async (event) => {
     console.debug("Event:", event);
   }
 
+  // Before authentication, rewrite paths to the index.html file
+  const request = event.Records[0].cf.request;
+  const uri = request.uri;
+
+  // Check whether the URI is missing a file name.
+  if (uri.endsWith('/')) {
+    request.uri += 'index.html';
+  }
+  // Check whether the URI is missing a file extension.
+  else if (!uri.includes('.')) {
+    request.uri += '/index.html';
+  }
+
+
   const authenticator = new Authenticator(authenticatorParams);
   return authenticator.handle(event);
 };
