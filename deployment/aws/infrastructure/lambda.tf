10884a22cd9f6d0319c3967c08fc40c69a8f6d14
Olivier Sels <olivier.sels@gmail.com>
2025-06-10 19:39:38 +0200

    Add renovate config

--- deployment/aws/infrastructure/lambda.tf
+++ deployment/aws/infrastructure/lambda.tf
@@ -37,6 +37,7 @@ resource "aws_lambda_function" "check_auth" {
 
   function_name = "wiki-check-auth-${local.seed_prefix}"
   role          = aws_iam_role.edge_lambda_role.arn
+  # renovate: datasource=endoflife-date depName=aws-lambda versioning=loose
   runtime       = "nodejs20.x"
   handler       = "bundle.handler"
 
